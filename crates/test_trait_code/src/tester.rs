afd537471ac3e05d27335924d0f3dac911189505
oligami-0424 <s20032@tokyo.kosen-ac.jp>
2024-08-03 01:42:08 +0900

    save

--- crates/test_trait_code/src/tester.rs
+++ crates/test_trait_code/src/tester.rs
@@ -12,3 +12,35 @@ pub fn tester_2(test_setting_manual: TestSettingManual) {
     println!("{:?}", t);
     println!("{:?}", test_setting_manual);
 }
+
+pub struct Tester<T: TestSettingManualTy> {
+    _phantom: std::marker::PhantomData<T>,
+}
+
+impl<T: TestSettingManualTy> Tester<T> {
+    pub fn new() -> Self {
+        Self {
+            _phantom: std::marker::PhantomData,
+        }
+    }
+
+    pub fn tester(&self) {
+        let t = T::TEST_DATA;
+        println!("{:?}", t);
+    }
+}
+
+pub struct Tester2 {
+    test_setting_manual: TestSettingManual,
+}
+
+impl Tester2 {
+    pub fn new(test_setting_manual: TestSettingManual) -> Self {
+        Self { test_setting_manual }
+    }
+
+    pub fn tester(&self) {
+        let t = self.test_setting_manual.test_data;
+        println!("{:?}", t);
+    }
+}
