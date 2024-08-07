1522563d5c914ec8d9b315d0c0659943010b044a
oligami-0424 <s20032@tokyo.kosen-ac.jp>
2024-08-08 01:11:03 +0900

    save

--- crates/test_trait_code/src/setting.rs
+++ crates/test_trait_code/src/setting.rs
@@ -13,3 +13,15 @@ pub struct WindowSettingManualTy;
 impl ConstStruct<TestSettingManual> for WindowSettingManualTy {
     const __DATA: TestSettingManual = WINDOW_SETTING_MANUAL;
 }
+
+pub trait U32Ty {
+    const __DATA: u32;
+}
+
+impl<T: U32TyImpl> U32Ty for u32 {
+    const __DATA: u32 = T::__DATA;
+}
+
+pub trait U32TyImpl {
+    const __DATA: Self;
+}
