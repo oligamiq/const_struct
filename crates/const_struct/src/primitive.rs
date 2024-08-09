06e1448369d2b1c8a0f4b35f7d06185dd4283528
oligami <nziq53@gmail.com>
2024-08-09 13:29:49 +0900

    save

--- crates/const_struct/src/primitive.rs
+++ crates/const_struct/src/primitive.rs
@@ -1,5 +1,6 @@
 use core::mem::transmute;
 use paste::paste;
+use crate::ConstStructTraits;
 
 macro_rules! PrimTraitBySizes {
     ($size:literal, $($name:ident),*) => {
@@ -23,6 +24,10 @@ macro_rules! PrimTraitBySizes {
                     const __DATA: $name = unsafe { transmute(T) };
                 }
 
+                impl<U: [<$name:camel Ty>], const T: $base> ConstStructTraits<[<$name:camel>]<T>> for U {
+                    const __DATA: [<$name:camel>]<T> = [<$name:camel>]::<T>;
+                }
+
                 #[macro_export]
                 macro_rules! [<$name:camel>] {
                     ($value:expr) => {
@@ -41,6 +46,28 @@ PrimTraitBySizes!(64, f64, u64, i64);
 PrimTraitBySizes!(128, u128, i128);
 PrimTraitBySizes!(usize, usize, isize);
 
+pub trait OptionTy<T: ConstStructTraits<T>, U> {
+    type TYPE: U;
+
+    const __DATA: Option<T>;
+    const VALUE: Option<T> = Self::__DATA;
+}
+
+pub struct OptionImpl<const B: bool, T: ConstStructTraits<T>> {
+    __phantom: core::marker::PhantomData<T>,
+}
+
+impl<const B: bool, T: ConstStructTraits<T>> OptionTy<T> for OptionImpl<B, T> {
+    const __DATA: Option<T> = if B { Some(T::__DATA) } else { None };
+}
+
+#[macro_export]
+macro_rules! Some {
+    ($value:ty) => {
+        OptionImpl<true, $value>
+    };
+}
+
 pub const fn tester_inner<T: F32Ty>() -> f32 {
     T::VALUE
 }
@@ -49,10 +76,14 @@ pub const fn tester_inner_u32<T: U32Ty>() -> u32 {
     T::VALUE
 }
 
+pub const fn tester_inner_option<T: OptionTy<F>, F: F32Ty + ConstStructTraits<F>>() -> Option<f32> {
+    let s = T::__DATA.take();
+}
+
 #[test]
 pub fn call_tester() {
     let s = F32!(-0.5);
     debug_assert_eq!(core::mem::size_of_val(&s), 0);
     debug_assert_eq!(tester_inner::<F32!(-0.5)>(), -0.5);
-    debug_assert_eq!(tester_inner::<F32!(-25.333)>(), -25.333);
+    debug_assert_eq!(tester_inner_option::<Some!(F32!(-25.333))>(), -25.333);
 }
