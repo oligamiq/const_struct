1304363c922209dde3f3c4e88506e69d2957b919
oligami <nziq53@gmail.com>
2024-08-09 11:57:01 +0900

    chore: Update dependencies and add 'paste' crate update primitive.rs

--- crates/const_struct/src/primitive.rs
+++ crates/const_struct/src/primitive.rs
@@ -1,30 +1,58 @@
-#![allow(unused)]
-
 use core::mem::transmute;
+use paste::paste;
 
-pub trait F32Ty {
-    const __DATA: f32;
-    const VALUE: f32 = Self::__DATA;
-}
+macro_rules! PrimTraitBySizes {
+    ($size:literal, $($name:ident),*) => {
+        $(
+            paste! {
+                PrimTraitBySizes!([<u $size>], $name);
+            }
+        )*
+    };
+    ($base:ident, $($name:ident),*) => {
+        $(
+            paste! {
+                pub trait [<$name:camel Ty>] {
+                    const __DATA: $name;
+                    const VALUE: $name = Self::__DATA;
+                }
 
-pub struct F32<const T: u32>;
+                pub struct [<$name:camel>]<const T: $base>;
 
-impl<const T: u32> F32Ty for F32<T> {
-    const __DATA: f32 = unsafe { transmute(T) };
-}
+                impl<const T: $base> [<$name:camel Ty>] for [<$name:camel>]<T> {
+                    const __DATA: $name = unsafe { transmute(T) };
+                }
 
-macro_rules! F32 {
-    ($value:expr) => {
-        F32::<{ unsafe { transmute(($value) as f32) } }>
+                #[macro_export]
+                macro_rules! [<$name:camel>] {
+                    ($value:expr) => {
+                        [<$name:camel>]::<{ unsafe { core::mem::transmute::<$name, $base>(($value)) } }>
+                    };
+                }
+            }
+        )*
     };
 }
 
+PrimTraitBySizes!(8, u8, i8, bool);
+PrimTraitBySizes!(16, u16, i16);
+PrimTraitBySizes!(32, f32, u32, i32, char);
+PrimTraitBySizes!(64, f64, u64, i64);
+PrimTraitBySizes!(128, u128, i128);
+PrimTraitBySizes!(usize, usize, isize);
+
 pub const fn tester_inner<T: F32Ty>() -> f32 {
-    T::__DATA
+    T::VALUE
+}
+
+pub const fn tester_inner_u32<T: U32Ty>() -> u32 {
+    T::VALUE
 }
 
 #[test]
 pub fn call_tester() {
+    let s = F32!(-0.5);
+    debug_assert_eq!(core::mem::size_of_val(&s), 0);
     debug_assert_eq!(tester_inner::<F32!(-0.5)>(), -0.5);
     debug_assert_eq!(tester_inner::<F32!(-25.333)>(), -25.333);
 }
--- crates/test_trait_code/src/primitive.rs
+++ crates/test_trait_code/src/primitive.rs
@@ -4,14 +4,13 @@ use std::mem::transmute;
 
 pub trait F32Ty {
     const __DATA: f32;
-    const VALUE: f32;
+    const VALUE: f32 = Self::__DATA;
 }
 
 pub struct F32<const T: u32>;
 
 impl<const T: u32> F32Ty for F32<T> {
     const __DATA: f32 = unsafe { transmute(T) };
-    const VALUE: f32 = Self::__DATA;
 }
 
 macro_rules! F32 {
@@ -29,3 +28,25 @@ pub fn call_tester() {
     debug_assert_eq!(tester_inner::<F32!(-0.5)>(), -0.5);
     debug_assert_eq!(tester_inner::<F32!(-25.333)>(), -25.333);
 }
+
+// struct RestrictInner<const B: bool>;
+// trait Restrict {}
+// impl Restrict for RestrictInner<true> {}
+
+// pub trait F32Ty {
+//     const __DATA: f32;
+//     const VALUE: f32 = Self::__DATA;
+// }
+
+// pub struct Mem32<const T: u32>;
+
+// pub trait Mem32Ty<T> {
+//     const __DATA: T;
+//     const VALUE: T = Self::__DATA;
+// }
+
+// impl<U, const T: u32> Mem32Ty<U> for Mem32<T>
+//     where RestrictInner<{ core::mem::size_of::<U>() == 32 }>: Restrict
+// {
+//     const __DATA: U = unsafe { transmute(T) };
+// }
