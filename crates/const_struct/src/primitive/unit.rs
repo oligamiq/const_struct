25579d7b492e9c8a1475d97cb7174ce3a066e25a
oligami-0424 <s20032@tokyo.kosen-ac.jp>
2024-08-13 02:09:47 +0900

    chore: Add unit tests for UnitTy implementation

--- crates/const_struct/src/primitive/unit.rs
+++ crates/const_struct/src/primitive/unit.rs
@@ -13,3 +13,20 @@ impl PrimitiveTraits for () {
 impl UnitTy for () {
     const __DATA: () = ();
 }
+
+#[cfg(test)]
+mod tests {
+    use super::UnitTy;
+    use core::mem;
+
+    pub const fn tester_inner<T: UnitTy>() -> () {
+        T::VALUE
+    }
+
+    #[test]
+    fn call_tester() {
+        let s: () = unsafe { mem::zeroed() };
+        assert_eq!(core::mem::size_of_val(&s), 0);
+        assert_eq!(tester_inner::<()>(), ());
+    }
+}
