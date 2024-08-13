854d04d8b72b69aadd2bc8597fdcf775989fee7d
oligami-0424 <s20032@tokyo.kosen-ac.jp>
2024-08-13 14:54:01 +0900

    save

--- crates/const_struct/src/lib.rs
+++ crates/const_struct/src/lib.rs
@@ -1,4 +1,16 @@
 #![no_std]
+//! # const_struct
+//!
+//! This crate provides a way to create a struct with const generics and const values.
+//! ```
+//! use const_struct::{primitive::F32Ty, F32};
+//! pub fn tester<A: F32Ty>() {
+//!     println!("a: {:?}", A::__DATA);
+//! }
+//! fn main() {
+//!     tester::<F32!(0.5)>();
+//! }
+//! ```
 
 pub mod prelude;
 pub mod primitive;
