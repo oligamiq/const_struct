fa53242ec1b8791b691bdb1ebd22d1801cfac176
oligami <nziq53@gmail.com>
2024-08-25 05:18:45 +0900

    save

--- crates/const_struct_derive/src/parse_value/mod.rs
+++ crates/const_struct_derive/src/parse_value/mod.rs
@@ -2,7 +2,7 @@ use crate::{
     const_struct_derive::PathAndIdent, ident::AbsolutePathOrType, macro_alt::match_underscore_alt,
     rewriter::change_macro::Switcher,
 };
-use array::parse_value_array;
+// use array::parse_value_array;
 use parse::{discouraged::Speculative as _, Parse, ParseStream};
 use path::parse_value_path;
 use proc_macro2::{Span, TokenStream};
@@ -10,7 +10,7 @@ use punctuated::Punctuated;
 use syn::*;
 use tuple::parse_value_tuple;
 
-mod array;
+// mod array;
 mod path;
 pub mod struct_ty;
 mod tuple;
@@ -174,7 +174,7 @@ pub fn parse_value(input: Type, expr: Expr, additional_data: &AdditionData) -> R
     match input {
         Type::Tuple(tuple) => parse_value_tuple(tuple, expr, additional_data),
         Type::Path(path) => parse_value_path(path, expr, additional_data),
-        Type::Array(array) => parse_value_array(array, expr, additional_data),
+        // Type::Array(array) => parse_value_array(array, expr, additional_data),
         _ => Err(Error::new_spanned(input, "unsupported type")),
     }
 }
