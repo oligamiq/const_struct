5b61293d1530803df72778d5471622cd3a44ae81
oligami <nziq53@gmail.com>
2024-08-26 17:45:23 +0900

    save

--- crates/test_code/src/generics.rs
+++ crates/test_code/src/generics.rs
@@ -1,4 +1,3 @@
-use const_struct::call_with_generics;
 use const_struct::{const_struct, ConstStruct};
 use core::fmt::Debug;
 
@@ -21,19 +20,49 @@ pub fn tester_test_generics<const T: usize, S: Float + Copy + Debug, U: TestGene
     no_std_compat::println!("tester_test_generics: {:?}", U::__DATA);
 }
 
-#[test]
-fn test_generics() {
-    tester_test_generics::<7, f32, TestGenerics!(7, f32, TestGenerics { s: 0.0 })>();
-    call_with_generics!(tester_test_generics::<
-        TestGenerics!(7, f32, TestGenerics { s: 0.0 }),
-    >());
-    call_with_generics!(tester_test_generics::<
-        TestGenerics!(_, f32, TestGenerics::<7, f32> { s: 0.0 }),
-    >());
-    call_with_generics!(tester_test_generics::<
-        TestGenerics!(f32, TestGenerics::<7, f32> { s: 0.0 }),
-    >());
+#[cfg(test)]
+pub mod test {
+    use super::{tester_test_generics, TestGenerics, BTy};
 
-    call_with_generics!(tester_test_generics::<TestGenerics!(BTy)>());
-    tester_test_generics::<7, f32, BTy>();
+    #[test]
+    fn test_generics() {
+        tester_test_generics::<7, f32, TestGenerics!(7, f32, TestGenerics { s: 0.0 })>();
+        const_struct::call_with_generics!(tester_test_generics::<
+            TestGenerics!(7, f32, TestGenerics { s: 0.0 }),
+        >());
+        const_struct::call_with_generics!(tester_test_generics::<
+            TestGenerics!(_, f32, TestGenerics::<7, f32> { s: 0.0 }),
+        >());
+        const_struct::call_with_generics!(tester_test_generics::<
+            TestGenerics!(f32, TestGenerics::<7, f32> { s: 0.0 }),
+        >());
+
+        const_struct::call_with_generics!(tester_test_generics::<TestGenerics!(BTy)>());
+        tester_test_generics::<7, f32, BTy>();
+    }
+
+}
+
+#[cfg(test)]
+pub mod test2 {
+    use super::tester_test_generics;
+    use crate::generics::TestGenerics;
+
+    #[test]
+    fn test_generics() {
+        tester_test_generics::<7, f32, super::TestGenerics!(7, f32, TestGenerics { s: 0.0 })>();
+        const_struct::call_with_generics!(tester_test_generics::<
+            super::TestGenerics!(7, f32, TestGenerics { s: 0.0 }),
+        >());
+        const_struct::call_with_generics!(tester_test_generics::<
+            super::TestGenerics!(_, f32, TestGenerics::<7, f32> { s: 0.0 }),
+        >());
+        const_struct::call_with_generics!(tester_test_generics::<
+            super::TestGenerics!(f32, TestGenerics::<7, f32> { s: 0.0 }),
+        >());
+
+        const_struct::call_with_generics!(tester_test_generics::<super::TestGenerics!(super::BTy)>());
+        tester_test_generics::<7, f32, super::BTy>();
+    }
+
 }
