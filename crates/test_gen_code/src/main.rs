451f45166b9741ef97bac93cad987491425a4216
oligami <nziq53@gmail.com>
2024-08-28 05:09:39 +0900

    failed!

--- crates/test_gen_code/src/main.rs
+++ crates/test_gen_code/src/main.rs
@@ -20,7 +20,18 @@ impl<const N: usize> core::fmt::Debug for TestSetting<N> {
     }
 }
 
-pub fn tester<const N: usize, const N2: usize, A: TupleTy<(TestSetting<N>, TestSetting<N2>)>, const N3: usize, B: TupleTy<(f32, TestSetting<N3>)>>() {
+#[derive(ConstStruct)]
+pub struct WestSetting<const N: usize>;
+
+impl<const N: usize> core::fmt::Debug for WestSetting<N> {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        f.debug_struct("WestSetting")
+            .field("N", &N)
+            .finish()
+    }
+}
+
+pub fn tester<const N: usize, const N2: usize, A: TupleTy<(TestSetting<N>, WestSetting<N2>)>, const N3: usize, B: TupleTy<(f32, TestSetting<N3>)>>() {
     println!("a: {:?}", A::__DATA);
     println!("b: {:?}", B::__DATA);
 }
@@ -30,5 +41,5 @@ const B: TestSetting<0> = TestSetting;
 
 #[test]
 fn main() {
-    call_with_generics!(tester::<(TestSetting!(BTy), TestSetting!(TestSetting::<2>)), (F32!(0.5), TestSetting!(8, TestSetting))>());
+    call_with_generics!(tester::<(TestSetting!(BTy), WestSetting!(WestSetting::<2>)), (F32!(0.5), TestSetting!(8, TestSetting))>());
 }
